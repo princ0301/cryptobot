732be5d0ed9e93b888117dbec601075aa522e49f
princ0301 <prince07190@gmail.com>
2026-05-12 03:01:36 +0530

    package

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: apiBaseUrl,
   timeout: 15000,
 })
 
